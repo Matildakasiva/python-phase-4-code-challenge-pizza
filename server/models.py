c1d222d60b0719be5e4b15864d3936ea54175c46
Matilda <ogarimatilda@gmail.com>
2024-07-01 10:00:57 +0300

    relationships and serialization

--- server/models.py
+++ server/models.py
@@ -21,9 +21,12 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    pizzas = db.relationship(
+        'RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
 
     # add serialization rules
-
+    serialize_only = ('id', 'name', 'address', 'pizzas')
+    
     def __repr__(self):
         return f"<Restaurant {self.name}>"
 
@@ -36,8 +39,12 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant = db.relationship(
+        'RestaurantPizza', back_populates= 'pizza', cascade= 'all, delete-orphan'
+    )
 
     # add serialization rules
+    serialize_only = ('id', 'name', 'ingredients', 'restaurants')
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,10 +55,16 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    # The foreign keys
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
 
     # add relationships
+    pizza = db.relationship('pizza', back_populates=('RestaurantPizza'))
+    restaurant = db.relationship('retaurant', back_populates=('RestaurantPizza'))
 
     # add serialization rules
+    serialize_only = ('id', 'price', 'restaurant', 'pizzas')
 
     # add validation
 
